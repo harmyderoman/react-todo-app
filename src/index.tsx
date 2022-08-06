1261d2cd0367775a6c16e72654175a4dfc68bd42
harmyderoman <romangarmider@gmail.com>
2022-08-06 13:31:43 +0300

    add router, redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import * as React from 'react'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
+import { BrowserRouter } from 'react-router-dom'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
