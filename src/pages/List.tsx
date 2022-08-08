bb9a2e514551668574a7722acfc4366c80708f20
harmyderoman <romangarmider@gmail.com>
2022-08-08 22:43:18 +0300

    List: add list of notes

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import type { Todo, Note } from 'models'
+import TodoItem from 'components/TodoItem'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  addNote,
+  deleteNote,
+  updateNote,
+  selectNotes
+} from './../store/notesSlice'
+
 function List() {
-  return <h1 className="text-2xl">List</h1>
+  const notes: Note[] = useSelector(selectNotes)
+  useEffect(() => {
+    console.log('notes:', notes)
+  })
+  return (
+    <>
+      <h1 className="text-2xl">List of Notes:</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <NavLink type="button" to={`note/${note.id}`}>
+              {note.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default List
