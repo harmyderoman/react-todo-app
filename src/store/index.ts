61a445a971faf091f14e94bba5fb96f13f6638aa
harmyderoman <romangarmider@gmail.com>
2022-08-20 00:11:36 +0300

    add redux-localstorage-simple

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notesReducer from './notesSlice'
 
+import { save, load } from 'redux-localstorage-simple'
+
 export default configureStore({
   reducer: {
     notes: notesReducer
-  }
+  },
+  middleware: [save()],
+  preloadedState: load()
 })
