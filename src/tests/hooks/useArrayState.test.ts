039106cbcbe1822fdc3046c3664ed41aa18589fa
harmyderoman <romangarmider@gmail.com>
2023-04-15 15:35:07 +0300

    fix test utils imports

--- src/tests/hooks/useArrayState.test.ts
+++ src/tests/hooks/useArrayState.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 import useArrayState from 'hooks/useArrayState'
 
 describe('useArrayState', () => {
